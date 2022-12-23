9b14b74a3d665cc47962a44538803d5abfd9939b
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2022-12-23 13:52:34 +0900

    Fix build.rs

--- nvidia-deepstream-sys/build.rs
+++ nvidia-deepstream-sys/build.rs
@@ -10,9 +10,11 @@ fn main() {
 
     println!("cargo:rustc-link-search=/opt/nvidia/deepstream/deepstream-{}/lib/", NVDS_VER);
     for path in &pk.link_paths {
-        println!("cargo:rustc-link-search=native={:?}", path);
+        println!("cargo:rustc-link-search={:?}", path);
     }
-    println!("cargo:rustc-link-lib=nvdsgst_meta nvds_meta nvds_yml_parser");
+    println!("cargo:rustc-link-lib=dylib=nvdsgst_meta");
+    println!("cargo:rustc-link-lib=dylib=nvds_meta");
+    println!("cargo:rustc-link-lib=dylib=nvds_yml_parser");
     println!("cargo:rerun-if-changed=wrapper.h");
 
     let mut bindings = bindgen::Builder::default()
