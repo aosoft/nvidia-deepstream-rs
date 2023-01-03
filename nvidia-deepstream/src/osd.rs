e13fb019978c7699324ef98779814f5e304869ad
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2023-01-03 18:36:23 +0900

    Implement osd.rs

--- nvidia-deepstream/src/osd.rs
+++ nvidia-deepstream/src/osd.rs
@@ -3,14 +3,18 @@ use gstreamer::glib;
 use std::ffi::CStr;
 
 #[repr(u32)]
+#[derive(Default)]
 pub enum Mode {
+    #[default]
     CPU = nvidia_deepstream_sys::NvOSD_Mode_MODE_CPU as _,
     GPU = nvidia_deepstream_sys::NvOSD_Mode_MODE_GPU as _,
     Hardware = nvidia_deepstream_sys::NvOSD_Mode_MODE_HW as _,
 }
 
 #[repr(u32)]
+#[derive(Default)]
 pub enum ArrowHeadDirection {
+    #[default]
     Start = nvidia_deepstream_sys::NvOSD_Arrow_Head_Direction_START_HEAD as _,
     End = nvidia_deepstream_sys::NvOSD_Arrow_Head_Direction_END_HEAD as _,
     Both = nvidia_deepstream_sys::NvOSD_Arrow_Head_Direction_BOTH_HEAD as _,
@@ -76,7 +80,7 @@ pub struct FontParamsBuilder {
 }
 
 impl FontParamsBuilder {
-    pub fn new() -> FontParamsBuilder {
+    pub fn new() -> Self {
         FontParamsBuilder {
             font_name: None,
             font_size: None,
@@ -151,7 +155,7 @@ pub struct TextParamsBuilder {
 }
 
 impl TextParamsBuilder {
-    pub fn new() -> TextParamsBuilder {
+    pub fn new() -> Self {
         TextParamsBuilder {
             display_text: None,
             display_text_ref: None,
@@ -297,7 +301,7 @@ pub struct RectParamsBuilder {
 }
 
 impl RectParamsBuilder {
-    pub fn new() -> RectParamsBuilder {
+    pub fn new() -> Self {
         RectParamsBuilder {
             left: None,
             top: None,
@@ -398,6 +402,56 @@ impl MaskParams {
     }
 }
 
+pub struct MaskParamsBuilder {
+    data: Option<&'static [f32]>,
+    threshold: Option<f32>,
+    width: Option<u32>,
+    height: Option<u32>,
+}
+
+impl MaskParamsBuilder {
+    pub fn new() -> Self {
+        MaskParamsBuilder {
+            data: None,
+            threshold: None,
+            width: None,
+            height: None,
+        }
+    }
+
+    pub fn data(mut self, data: &'static [f32]) -> Self {
+        self.data = Some(data);
+        self
+    }
+
+    pub fn threshold(mut self, threshold: f32) -> Self {
+        self.threshold = Some(threshold);
+        self
+    }
+
+    pub fn width(mut self, width: u32) -> Self {
+        self.width = Some(width);
+        self
+    }
+
+    pub fn height(mut self, height: u32) -> Self {
+        self.height = Some(height);
+        self
+    }
+
+    pub fn build(self) -> MaskParams {
+        MaskParams::from_native_type(
+            nvidia_deepstream_sys::NvOSD_MaskParams {
+                data: self.data.map_or(std::ptr::null_mut(), |v| v.as_ptr() as _),
+                size: self.data.map_or(0, |v| v.len() as _),
+                threshold: self.threshold.unwrap_or_default(),
+                width: self.width.unwrap_or_default(),
+                height: self.height.unwrap_or_default(),
+            }
+        )
+    }
+}
+
 crate::wrapper_impl!(LineParams, nvidia_deepstream_sys::NvOSD_LineParams);
 
 impl LineParams {
@@ -425,6 +479,72 @@ impl LineParams {
         ColorParams::from_native_type_ref(&self.as_native_type_ref().line_color)
     }
 }
+
+pub struct LineParamsBuilder {
+    x1: Option<u32>,
+    y1: Option<u32>,
+    x2: Option<u32>,
+    y2: Option<u32>,
+    line_width: Option<u32>,
+    line_color: Option<ColorParams>,
+}
+
+impl LineParamsBuilder {
+    pub fn new() -> Self {
+        LineParamsBuilder {
+            x1: None,
+            y1: None,
+            x2: None,
+            y2: None,
+            line_width: None,
+            line_color: None,
+        }
+    }
+
+    pub fn x1(mut self, value: u32) -> Self {
+        self.x1 = Some(value);
+        self
+    }
+
+    pub fn y1(mut self, value: u32) -> Self {
+        self.y1 = Some(value);
+        self
+    }
+
+    pub fn x2(mut self, value: u32) -> Self {
+        self.x2 = Some(value);
+        self
+    }
+
+    pub fn y2(mut self, value: u32) -> Self {
+        self.y2 = Some(value);
+        self
+    }
+
+    pub fn line_width(mut self, width: u32) -> Self {
+        self.line_width = Some(width);
+        self
+    }
+
+    pub fn line_color(mut self, color: ColorParams) -> Self {
+        self.line_color = Some(color);
+        self
+    }
+
+    pub fn build(self) -> LineParams {
+        LineParams::from_native_type(
+            nvidia_deepstream_sys::NvOSD_LineParams {
+                x1: self.x1.unwrap_or_default(),
+                y1: self.y1.unwrap_or_default(),
+                x2: self.x2.unwrap_or_default(),
+                y2: self.y2.unwrap_or_default(),
+                line_width: self.line_width.unwrap_or_default(),
+                line_color: self.line_color.unwrap_or(ColorParams::white()).as_native_type(),
+            }
+        )
+    }
+}
+
 
 crate::wrapper_impl!(ArrowParams, nvidia_deepstream_sys::NvOSD_ArrowParams);
 
@@ -458,6 +578,81 @@ impl ArrowParams {
     }
 }
 
+pub struct ArrowParamsBuilder {
+    x1: Option<u32>,
+    y1: Option<u32>,
+    x2: Option<u32>,
+    y2: Option<u32>,
+    arrow_width: Option<u32>,
+    arrow_head: Option<ArrowHeadDirection>,
+    arrow_color: Option<ColorParams>,
+}
+
+impl ArrowParamsBuilder {
+    pub fn new() -> Self {
+        ArrowParamsBuilder {
+            x1: None,
+            y1: None,
+            x2: None,
+            y2: None,
+            arrow_width: None,
+            arrow_head: None,
+            arrow_color: None,
+        }
+    }
+
+    pub fn x1(mut self, value: u32) -> Self {
+        self.x1 = Some(value);
+        self
+    }
+
+    pub fn y1(mut self, value: u32) -> Self {
+        self.y1 = Some(value);
+        self
+    }
+
+    pub fn x2(mut self, value: u32) -> Self {
+        self.x2 = Some(value);
+        self
+    }
+
+    pub fn y2(mut self, value: u32) -> Self {
+        self.y2 = Some(value);
+        self
+    }
+
+    pub fn arrow_width(mut self, width: u32) -> Self {
+        self.arrow_width = Some(width);
+        self
+    }
+
+    pub fn arrow_head(mut self, head: ArrowHeadDirection) -> Self {
+        self.arrow_head = Some(head);
+        self
+    }
+
+    pub fn arrow_color(mut self, color: ColorParams) -> Self {
+        self.arrow_color = Some(color);
+        self
+    }
+
+    pub fn build(self) -> ArrowParams {
+        ArrowParams::from_native_type(
+            nvidia_deepstream_sys::NvOSD_ArrowParams {
+                x1: self.x1.unwrap_or_default(),
+                y1: self.y1.unwrap_or_default(),
+                x2: self.x2.unwrap_or_default(),
+                y2: self.y2.unwrap_or_default(),
+                arrow_width: self.arrow_width.unwrap_or_default(),
+                arrow_head: unsafe { std::mem::transmute(self.arrow_head.unwrap_or_default()) },
+                arrow_color: self.arrow_color.unwrap_or(ColorParams::white()).as_native_type(),
+                reserved: 0,
+            }
+        )
+    }
+}
+
+
 crate::wrapper_impl!(CircleParams, nvidia_deepstream_sys::NvOSD_CircleParams);
 
 impl CircleParams {
@@ -485,5 +680,62 @@ impl CircleParams {
         } else {
             None
         }
+    }
+}
+
+pub struct CircleParamsBuilder {
+    xc: Option<u32>,
+    yc: Option<u32>,
+    radius: Option<u32>,
+    circle_color: Option<ColorParams>,
+    bg_color: Option<ColorParams>,
+}
+
+impl CircleParamsBuilder {
+    pub fn new() -> Self {
+        CircleParamsBuilder {
+            xc: None,
+            yc: None,
+            radius: None,
+            circle_color: None,
+            bg_color: None,
+        }
+    }
+
+    pub fn xc(mut self, value: u32) -> Self {
+        self.xc = Some(value);
+        self
+    }
+
+    pub fn yc(mut self, value: u32) -> Self {
+        self.yc = Some(value);
+        self
+    }
+
+    pub fn radius(mut self, radius: u32) -> Self {
+        self.radius = Some(radius);
+        self
+    }
+
+    pub fn circle_color(mut self, color: ColorParams) -> Self {
+        self.circle_color = Some(color);
+        self
+    }
+
+    pub fn bg_color(mut self, color: Option<ColorParams>) -> Self {
+        self.bg_color = color;
+        self
+    }
+
+    pub fn build(self) -> CircleParams {
+        CircleParams::from_native_type(nvidia_deepstream_sys::NvOSD_CircleParams {
+            xc: self.xc.unwrap_or_default(),
+            yc: self.yc.unwrap_or_default(),
+            radius: self.radius.unwrap_or_default(),
+            circle_color: self.circle_color.unwrap_or(ColorParams::white()).as_native_type(),
+            has_bg_color: if self.bg_color.is_some() { 1 } else { 0 },
+            bg_color: self.bg_color.unwrap_or_default().as_native_type(),
+            reserved: 0,
+        })
     }
 }
