ee0f50570c736efffcb2ccbc55762b2d25b7db81
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2023-01-15 01:26:10 +0900

    Add nvds_audio_meta.h

--- nvidia-deepstream-sys/wrapper.h
+++ nvidia-deepstream-sys/wrapper.h
@@ -2,3 +2,6 @@
 #include <nvbufsurface.h>
 //#include <nvbufsurftransform.h>   //  pending
 #include <nvds_yml_parser.h>
+
+#include <nvds_audio_meta.h>
+
--- nvidia-deepstream/src/meta/audio.rs
+++ nvidia-deepstream/src/meta/audio.rs
@@ -1,1 +1,3 @@
 use crate::WrapperExt;
+
+crate::wrapper_impl!(AudioFrameMeta, nvidia_deepstream_sys::NvDsAudioFrameMeta);
