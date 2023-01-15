dc24bb445e21737aa4602d9dc744fed613113169
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2023-01-15 13:18:11 +0900

    Implement audio.rs

--- nvidia-deepstream/src/meta/audio.rs
+++ nvidia-deepstream/src/meta/audio.rs
@@ -1,4 +1,6 @@
-use crate::meta::BaseMeta;
+use std::ffi::CStr;
+use std::ptr::NonNull;
+use crate::meta::{BaseMeta, ClassifierMeta, MetaList, UserMeta};
 use crate::WrapperExt;
 
 #[repr(u32)]
@@ -108,6 +110,78 @@ impl AudioFrameMeta {
 
     pub fn base_meta_mut(&mut self) -> &mut BaseMeta {
         BaseMeta::from_native_type_mut(&mut self.as_native_type_mut().base_meta)
+    }
+
+    pub fn pad_index(&self) -> u32 {
+        self.as_native_type_ref().pad_index
+    }
+
+    pub fn batch_id(&self) -> u32 {
+        self.as_native_type_ref().batch_id
+    }
+
+    pub fn frame_num(&self) -> i32 {
+        self.as_native_type_ref().frame_num
+    }
+
+    pub fn buf_pts(&self) -> u64 {
+        self.as_native_type_ref().buf_pts
+    }
+
+    pub fn ntp_timestamp(&self) -> u64 {
+        self.as_native_type_ref().ntp_timestamp
+    }
+
+    pub fn source_id(&self) -> u32 {
+        self.as_native_type_ref().source_id
+    }
+
+    pub fn num_samples_per_frame(&self) -> i32 {
+        self.as_native_type_ref().num_samples_per_frame
+    }
+
+    pub fn sample_rate(&self) -> u32 {
+        self.as_native_type_ref().sample_rate
+    }
+
+    pub fn num_channels(&self) -> u32 {
+        self.as_native_type_ref().num_channels
+    }
+
+    pub fn format(&self) -> AudioFormat {
+        unsafe { std::mem::transmute(self.as_native_type_ref().format) }
+    }
+
+    pub fn layout(&self) -> AudioLayout {
+        unsafe { std::mem::transmute(self.as_native_type_ref().layout) }
+    }
+
+    pub fn infer_done(&self) -> bool {
+        self.as_native_type_ref().bInferDone != 0
+    }
+
+    pub fn class_id(&self) -> i32 {
+        self.as_native_type_ref().class_id
+    }
+
+    pub fn confidence(&self) -> f32 {
+        self.as_native_type_ref().confidence
+    }
+
+    pub fn class_label(&self) -> &CStr {
+        unsafe { CStr::from_ptr(self.as_native_type_ref().class_label.as_ptr()) }
+    }
+
+    pub fn classifier_meta_list(&self) -> MetaList<ClassifierMeta> {
+        MetaList::<ClassifierMeta>::new(NonNull::new(self.as_native_type_ref().classifier_meta_list).unwrap())
+    }
+
+    pub fn frame_user_meta_list(&self) -> MetaList<UserMeta> {
+        MetaList::<UserMeta>::new(NonNull::new(self.as_native_type_ref().frame_user_meta_list).unwrap())
+    }
+
+    pub fn misc_frame_info(&self) -> [i64; 4usize] {
+        self.as_native_type_ref().misc_frame_info
     }
 
 }
